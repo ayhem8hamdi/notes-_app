08506b8fbb6cb3768e62f1e2b357557079e19c9f
ayheem8hamdi <ayhemhamdi24@gmail.com>
2024-09-05 19:07:59 +0100

    Create Note States

---
+++ lib/Cubits/NoteCubit/note_cubit_states.dart
@@ -0,0 +1,14 @@
+import 'package:note_app/Models/note_model.dart';
+
+class NoteStates {}
+
+class NoteIntital extends NoteStates {}
+
+class NoteLoading extends NoteStates {}
+
+class NoteSucces extends NoteStates {
+  final List<NoteModel> note;
+  NoteSucces(this.note);
+}
+
+class NoteFailed extends NoteStates {}
